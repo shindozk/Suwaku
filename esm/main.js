b022f98244bad079f1817b5da3ca96222e4e5f81
ShindoZk <77684277+shindozk@users.noreply.github.com>
2025-10-23 19:36:37 -0300

    Delete esm directory

--- esm/main.js
+++
@@ -1,8 +0,0 @@
-import {YukufyClient} from './src/utils/Player.js';
-import "./src/scripts/Update.js";
-
-export {
-    YukufyClient
-};
-
-//© 2025 Yukufy Player Music - Kandaraku Studios | Owner - Developer: shindozk

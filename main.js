2a2b99db5f2050c568405ad916062f1004f46239
ShindoZk <77684277+shindozk@users.noreply.github.com>
2025-10-23 19:37:34 -0300

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-const {YukufyClient} = require("./src/utils/Player");
-require("./src/utils/FFmpegManager");
-require("./src/scripts/Update");
-
-module.exports = {
-    YukufyClient
-};
-
-//© 2025 Yukufy Player Music - Kandaraku Studios | Owner - Developer: shindozk
